3aab9786930da7a14674446882597ee08fc8b7d1
Tom <2048137831@qq.com>
2021-10-02 21:37:42 +0800

    use http client customed to request mobile imooc

--- src/http_try/http_server.go
+++ src/http_try/http_server.go
@@ -7,16 +7,28 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("http://www.imooc.com")
+	req, err := http.NewRequest(http.MethodGet, "http://www.imooc.com", nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("req: ", req)
+			return nil
+		},
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	respContent, err2 := httputil.DumpResponse(resp, true)
-	if err2 != nil {
-		panic(err2)
+	respContent, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("%s\n", respContent)
+	fmt.Printf("%s\n", respContent[:1])
 }
